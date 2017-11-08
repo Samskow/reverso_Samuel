0dbb80112e99b06e65f4d12a138b6f502bc0be6f
Moi <samuel@MacBook-Pro-de-samuel.local>
2017-11-08 01:00:07 -0500

    v2

--- testnamesbyalphabet/ViewController.swift
+++ testnamesbyalphabet/ViewController.swift
@@ -9,8 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    var resultToDisplay:String = ""
+    var arrOfNames = ["Armand","Bart","Annie","Caro","Alain","Brad"]
+    var namesByAlphabet: [String.Element:[String]]!
 
+    
+    
+    
     override func viewDidLoad() {
+        namesByAlphabet = Dictionary(grouping:arrOfNames,by:{$0.first!})
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +32,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func search(_ sender: UITextField) {
+        resultToDisplay = ""
+                for i in namesByAlphabet {
+                    if textField.text == String(namesByAlphabet.keys){
+                        print("ok")
+                    }
+                    
+            
+            
+        }
+        
+    }
+    
 }
 
