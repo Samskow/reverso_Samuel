97fe71185af3a0d92026bbf166493d8d436799e2
Moi <samuel@MacBook-Pro-de-samuel.local>
2017-11-10 00:48:05 -0500

    vOK

--- testnamesbyalphabet/ViewController.swift
+++ testnamesbyalphabet/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
-   
+    
     
     
     @IBOutlet weak var textfield: UITextField!
@@ -20,72 +20,80 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     var arrOfNames = ["Armand","Bart","Annie","Caro","Alain","Brad"]
     var motsFrParLettre: [String.Element:[String]]!
     var namesByAlphabet: [String.Element:[String]]!
-    var dictFrAn = ["chat":"cat","chien":"dog"]
-    var theKey = ""
-    var theValue = ""
-
+    var dictFrAn = ["chat":"cat","chien":"dog","loup":"wolf","ours":"bear"]
+    var theKey = String()
+    var theValue = String()
+    var listElementCorrespondants = [String]()
+    
     
     override func viewDidLoad() {
-//        namesByAlphabet = Dictionary(grouping:arrOfNames,by:{$0.first!})
+        //        namesByAlphabet = Dictionary(grouping:arrOfNames,by:{$0.first!})
         
-           motsFrParLettre = Dictionary(grouping:arrOfNames,by:{$0.first!})
-         print(motsFrParLettre)
+        motsFrParLettre = Dictionary(grouping:arrOfNames,by:{$0.first!})
+        print(motsFrParLettre)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     //----------HOME---------------------------
     
     @IBAction func traduire(_ sender: UIButton) {
         
-        var premiereLettre = ((textfield.text!).characters.first?.description ?? "")
+        for (keys,value) in dictFrAn {
+            
+            if (textfield.text)?.uppercased() == (keys).uppercased(){
+                resultat.text = value
+            }
+        }
+    }
+    
+    //-------------------------------------
+    @IBAction func controleDuTextField(_ sender: UITextField) {
+        let premiereLettre = ((textfield.text!).characters.first?.description ?? "")
         
         print(premiereLettre)
         
         for (keys,value) in dictFrAn {
+            
             print(keys,value,"Avant")
             if premiereLettre.uppercased() == (keys.characters.first?.description ?? "").uppercased(){
                 print("ouiii")
-                print(keys,value,"Apres")
-                
-                
+                print(keys,"commence par",premiereLettre)
+                //                theKey = keys
+                //                theValue = "ouiiii"
+                listElementCorrespondants.append(keys)
+                tableViewHome.reloadData()
             }
-            
-        }
-        
-        //        dictFrAn = [textfield.text!:textfield.text!] //pour ajouter une keys:value
-        tableViewHome.reloadData()
+            // else{
+            // listElementCorrespondants = [String]()
+            // tableViewHome.reloadData()
+            //   }
+            }
     }
-    
-    
-    
     //-----------------------------------------------
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dictFrAn.count
+        return listElementCorrespondants.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default,reuseIdentifier: nil)
-        //        cell.textLabel?.text = "MOTS"
-//        let theKey = [String](dictFrAn.keys)[indexPath.row]
-//        theValue = [String](dictFrAn.values)[indexPath.row]
         
-        theKey = textfield.text!
-        theValue = [String](dictFrAn.values)[indexPath.row]
-        cell.textLabel?.text = "\(theKey) = \(theValue)"
+        cell.textLabel?.text = listElementCorrespondants[indexPath.row]
         return cell
     }
-
-    //----------AJOUTER---------------------------
+    
+    //----------MEMO---------------------------
     
     // Utiliser le userDefault comme  variable globale et distribuer au différente interface
-
+    
+    //        dictFrAn = [textfield.text!:textfield.text!] //pour ajouter une keys:value
+    
     
     
     
