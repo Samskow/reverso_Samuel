7a393f8c739216627d95455d32c88614aace9b9c
Moi <samuel@MacBook-Pro-de-samuel.local>
2017-11-10 19:26:30 -0500

    VOK2

--- testnamesbyalphabet/ViewController.swift
+++ testnamesbyalphabet/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate{
     
     
     
@@ -17,20 +17,23 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     @IBOutlet weak var resultat: UILabel!
     
     var resultToDisplay:String = ""
-    var arrOfNames = ["Armand","Bart","Annie","Caro","Alain","Brad"]
-    var motsFrParLettre: [String.Element:[String]]!
+    
+    
     var namesByAlphabet: [String.Element:[String]]!
-    var dictFrAn = ["chat":"cat","chien":"dog","loup":"wolf","ours":"bear"]
+    
+    
     var theKey = String()
     var theValue = String()
     var listElementCorrespondants = [String]()
-    
+    var dictFrAn = ["chat":"cat","chien":"dog","loup":"wolf","ours":"bear","tigre":"tiger","perroquet":"parrot","canard":"duck"]
+    var tabMotsFr = ["chat","chien","loup","ours","tigre","perroquet"]
+    var tabMotsAn = ["cat","dog","wolf","bear","tiger","parrot"]
     
     override func viewDidLoad() {
         //        namesByAlphabet = Dictionary(grouping:arrOfNames,by:{$0.first!})
         
-        motsFrParLettre = Dictionary(grouping:arrOfNames,by:{$0.first!})
-        print(motsFrParLettre)
+        
+        textfield.placeholder = "Mot en français"
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -38,42 +41,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    //----------HOME---------------------------
-    
-    @IBAction func traduire(_ sender: UIButton) {
-        
-        for (keys,value) in dictFrAn {
-            
-            if (textfield.text)?.uppercased() == (keys).uppercased(){
-                resultat.text = value
-            }
-        }
-    }
-    
-    //-------------------------------------
-    @IBAction func controleDuTextField(_ sender: UITextField) {
-        let premiereLettre = ((textfield.text!).characters.first?.description ?? "")
-        
-        print(premiereLettre)
-        
-        for (keys,value) in dictFrAn {
-            
-            print(keys,value,"Avant")
-            if premiereLettre.uppercased() == (keys.characters.first?.description ?? "").uppercased(){
-                print("ouiii")
-                print(keys,"commence par",premiereLettre)
-                //                theKey = keys
-                //                theValue = "ouiiii"
-                listElementCorrespondants.append(keys)
-                tableViewHome.reloadData()
-            }
-            // else{
-            // listElementCorrespondants = [String]()
-            // tableViewHome.reloadData()
-            //   }
-            }
     }
     //-----------------------------------------------
     
@@ -83,16 +50,111 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default,reuseIdentifier: nil)
-        
         cell.textLabel?.text = listElementCorrespondants[indexPath.row]
         return cell
     }
+    //---------CHANGER DE LANGUE --------------------------
+    
+    @IBAction func choixDeLangue(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            textfield.text = ""
+            textfield.placeholder = "Mot en français"
+            listElementCorrespondants = [String]()
+            tableViewHome.reloadData()
+            dictFrAn = Dictionary(uniqueKeysWithValues: zip(tabMotsFr,tabMotsAn))
+            resultat.text = "Resultat"
+            
+        case 1:
+            textfield.text = ""
+            textfield.placeholder = "Mot en anglais"
+            listElementCorrespondants = [String]()
+            tableViewHome.reloadData()
+            dictFrAn = Dictionary(uniqueKeysWithValues: zip(tabMotsAn,tabMotsFr))
+            resultat.text = "Resultat"
+        default:
+            break
+        }
+        
+        
+    }
+    
+    
+    
+    //----------HOME---------------------------
+    
+    @IBAction func traduire(_ sender: UIButton) {
+        
+        resultat.text = ""
+        for (keys,value) in dictFrAn {
+            
+            if (textfield.text)?.uppercased() == (keys).uppercased(){
+                resultat.text = value
+                
+            }
+        }
+    }
+    
+    //-------------------------------------
+    @IBAction func controleDuTextField(_ sender: UITextField) {
+        
+        let premiereLettre = ((textfield.text!).characters.first?.description ?? "")
+        print(premiereLettre)
+        listElementCorrespondants = [String]()
+        
+        for (keys,_) in dictFrAn {
+            
+            if premiereLettre.uppercased() == (keys.characters.first?.description ?? "").uppercased(){
+                print(keys,"commence par",premiereLettre)
+                listElementCorrespondants.append(keys)
+                //sauvegarder
+                tableViewHome.reloadData()
+            }
+            else if textfield.text == "" {
+                
+                listElementCorrespondants = [String]()
+                tableViewHome.reloadData()
+            }
+            
+        }
+        print("tab est ",listElementCorrespondants)
+        
+        
+    }
+    
+    // pour recuperer un element selectionner
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        textfield.text = [String](dictFrAn.keys)[indexPath.row]
+        print("dictionnaire afficher", [String](dictFrAn.keys)[indexPath.row])
+        
+    }
+    
+    //    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    //        textField.resignFirstResponder()
+    //        return true
+    //    }
+    
+    //    let TEXT_FIELD_LIMIT = 1
+    //    func textField(_ _textFiel: UITextField,shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool{
+    //
+    //        return(textfield.text?.utf16.count ?? 0) + string.utf16.count - range.length <= TEXT_FIELD_LIMIT
+    //    }
+    
+    
+    
+    
     
     //----------MEMO---------------------------
     
     // Utiliser le userDefault comme  variable globale et distribuer au différente interface
     
     //        dictFrAn = [textfield.text!:textfield.text!] //pour ajouter une keys:value
+    
+    // quand on clique sur anglais on echange les key et value
     
     
     
